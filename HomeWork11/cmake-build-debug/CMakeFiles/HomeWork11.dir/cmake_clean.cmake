1d922a0f5cc2acb9da6a23f229af0fac212d96c3
Cuiods <cuihaoemail@163.com>
2019-02-20 17:26:00 +0800

    Back up file

---
+++ HomeWork11/cmake-build-debug/CMakeFiles/HomeWork11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork11.dir/main.cpp.obj"
+  "HomeWork11.pdb"
+  "HomeWork11.exe"
+  "HomeWork11.exe.manifest"
+  "libHomeWork11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
